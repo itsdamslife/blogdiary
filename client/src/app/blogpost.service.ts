d3c20bc2049c9f22535b599ef84b4c6e1dad1d9e
Damodar Shenoy <dshenoy@vmware.com>
2018-03-06 06:16:52 +0530

    Corrected the glitch in local storage saving

--- client/src/app/blogpost.service.ts
+++ client/src/app/blogpost.service.ts
@@ -11,12 +11,29 @@ export class BlogpostService {
   constructor() { }
 
   getPosts(): Observable<Blogpost[]> {
+    if (typeof (Storage) !== "undefined") {
+      var bps = localStorage.getItem("blogposts");
+      console.log(" blogposts from local storage : \n" + bps);
+      var bpsArray: Blogpost[] = JSON.parse(bps);
+      return of(bpsArray);
+    } else {
+      console.log("Sorry! No Web Storage support..");
+    }
     return of(BLOGPOSTS);
   }
 
   addPost(blogpost: Blogpost) {
     // add to the top of the list
-    BLOGPOSTS.unshift(blogpost);
+    // BLOGPOSTS.unshift(blogpost);
+    if (typeof (Storage) !== "undefined") {
+      var bps = localStorage.getItem("blogposts");
+      console.log(" blogposts from local storage : \n" + bps);
+      var bpsArray: Blogpost[] = JSON.parse(bps);
+      bpsArray.unshift(blogpost);
+      localStorage.setItem("blogposts", JSON.stringify(bpsArray));
+    } else {
+      console.log("Sorry! No Web Storage support..");
+    }
   }
   
 }
