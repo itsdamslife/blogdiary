e9fc10f2226654910dc26c42f5453fe9e1e20feb
Damodar Shenoy <dshenoy@vmware.com>
2018-03-08 06:39:09 +0530

    Switching between Read and Edit mode done. Cancel edit is done.

--- client/src/app/blogpost-detail/blogpost-detail.component.ts
+++ client/src/app/blogpost-detail/blogpost-detail.component.ts
@@ -12,11 +12,37 @@ import { Blogpost } from '../blogpost';
 export class BlogpostDetailComponent implements OnInit {
 
   blogpost: Blogpost;
+  blogpostBeforeEditing: Blogpost = new Blogpost();
+  isPostInEditMode: Boolean = false;
 
   constructor(private route: ActivatedRoute, private blogpostService: BlogpostService) { }
 
   ngOnInit() {
     this.getPost();
+  }
+
+  private switchToEditMode(shouldEdit: Boolean) {
+    if (shouldEdit) {
+      document.getElementById('titleField').removeAttribute('disabled');
+      document.getElementById('blogpost').removeAttribute('disabled');
+      document.getElementById('deleteButton').innerHTML = "Cancel";
+      document.getElementById('editButton').innerHTML = "Save";
+    } else {
+      document.getElementById('titleField').setAttribute("disabled", "disabled");
+      document.getElementById('blogpost').setAttribute("disabled", "disabled");
+      document.getElementById('deleteButton').innerHTML = "Delete";
+      document.getElementById('editButton').innerHTML = "Edit";
+    }
+  }
+
+  private backUpPostDetails() {
+    this.blogpostBeforeEditing.title = this.blogpost.title;
+    this.blogpostBeforeEditing.description = this.blogpost.description;
+  }
+
+  private restoreBackUpPost() {
+    this.blogpost.title = this.blogpostBeforeEditing.title;
+    this.blogpost.description = this.blogpostBeforeEditing.description;
   }
 
   getPost(): void {
@@ -31,10 +57,33 @@ export class BlogpostDetailComponent implements OnInit {
 
   editPost() {
     console.log("Edit post!!");
+
+    if(this.isPostInEditMode == false) {
+      this.backUpPostDetails();
+      this.isPostInEditMode = true;
+    } else {
+      this.isPostInEditMode = false;
+      // update the blogpost thru the service
+    }
+    this.switchToEditMode(this.isPostInEditMode);
+
   }
 
   deletePost() {
     console.log("Delete post!!");
+
+    if (this.isPostInEditMode == true) {
+      // Cancel operation
+      console.log("old blog post = " + this.blogpostBeforeEditing.title);
+      this.isPostInEditMode = false;
+      this.restoreBackUpPost();
+      this.blogpostBeforeEditing = new Blogpost();
+    } else {
+      // Delete operation
+      this.isPostInEditMode = true;
+      // Delete the post thru service
+    }
+    this.switchToEditMode(this.isPostInEditMode);
   }
 
 }
